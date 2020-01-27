f976ea0bb6dd92db274af1f471e57a864f967c10
Theo Holmqvist Berlin <theo.holmqvist@gmail.com>
2020-01-27 14:45:02 +0100

    Vulkan renderer base class

---
+++ testbench-master/gl_testbench/Vulkan/VulkanRenderer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../Renderer.h"
+
+class VulkanRenderer : public Renderer
+{
+public:
+    VulkanRenderer();
+    ~VulkanRenderer();
+
+    Material* makeMaterial(const std::string& name);
+	Mesh* makeMesh();
+	VertexBuffer* makeVertexBuffer(size_t size, VertexBuffer::DATA_USAGE usage);
+	Texture2D* makeTexture2D();
+	Sampler2D* makeSampler2D();
+	RenderState* makeRenderState();
+	std::string getShaderPath();
+	std::string getShaderExtension();
+	ConstantBuffer* makeConstantBuffer(std::string NAME, unsigned int location);
+	Technique* makeTechnique(Material*, RenderState*);
+
+	//Renderer() { /*InitializeCriticalSection(&protectHere);*/ };
+	int initialize(unsigned int width = 800, unsigned int height = 600);
+	void setWinTitle(const char* title);
+	void present();
+	int shutdown();
+
+	void setClearColor(float, float, float, float);
+	void clearBuffer(unsigned int);
+	// can be partially overriden by a specific Technique.
+	void setRenderState(RenderState* ps);
+	// submit work (to render) to the renderer.
+	void submit(Mesh* mesh);
+	void frame();
+};
