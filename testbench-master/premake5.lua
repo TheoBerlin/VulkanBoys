21b83b4b77ee2d87bec0d1df60165a52151ded1d
ALEXANDER DAHLIN <aldb16@student.bth.se>
2020-01-27 15:05:14 +0100

    Added premake

---
+++ testbench-master/premake5.lua
@@ -0,0 +1,73 @@
+workspace "gl_Testbench"
+    startproject "gl_Testbench"
+	warnings "Extra"
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+	platforms
+	{
+        "x64",
+    }
+    
+	filter "configurations:Debug"
+		symbols "On"
+		runtime "Debug"
+		defines 
+		{
+			"DEBUG",
+		}
+	filter "configurations:Release"
+		symbols "On"
+		runtime "Release"
+		optimize "Full"
+		defines 
+		{ 
+			"NDEBUG",
+		}
+	filter {}
+
+    project "gl_Testbench"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++17"
+		systemversion "latest"
+        staticruntime "on"
+        location "gl_Testbench"
+        architecture "x64"
+
+		targetdir 	("Build/bin/" .. outputdir .. "/%{prj.name}")
+		objdir 		("Build/bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "gl_Testbench/**.h",
+            "gl_Testbench/**.cpp",
+        }
+
+        sysincludedirs
+        {
+            "%{wks.location.abspath}",
+            "GLEW/include",
+            "include",
+            "SDL/include",
+        }
+
+        libdirs
+        {
+            "GLEW/lib/Release/64",
+            "SDL/lib/64"
+        }
+
+        links
+        {
+            "SDL2",
+            "glew32"
+        }
+    project "*"
+
