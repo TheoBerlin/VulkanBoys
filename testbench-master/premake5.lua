d4f35493adfe6c2477cb6c741b8f2ae682f086d7
Herman <skumbananaaa@gmail.com>
2020-01-28 14:10:42 +0100

    Auto stash before merge of "master" and "origin/master"

--- testbench-master/premake5.lua
+++ testbench-master/premake5.lua
@@ -62,8 +62,8 @@ workspace "gl_Testbench"
 
         libdirs
         {
-			"C:/VulkanSDK/1.1.108.0/Lib",
-            "C:/VulkanSDK/1.1.130.0/Lib",
+            "C:/VulkanSDK/1.1.108.0/Lib",
+			"C:/VulkanSDK/1.1.130.0/Lib",
             "GLEW/lib/Release/64",
             "SDL/lib/64",
         }
