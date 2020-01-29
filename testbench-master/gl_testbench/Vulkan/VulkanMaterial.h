00dd9e6aa315289aadc6f4ca8aa7e6e80e3d5ca0
Alexander Dahlin <alexander.dahlins@gmail.com>
2020-01-29 14:38:41 +0100

    Added VulkanMaterial

---
+++ testbench-master/gl_testbench/Vulkan/VulkanMaterial.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Material.h"
+#include "Common.h"
+
+class VulkanMaterial : public Material
+{
+public:
+	VulkanMaterial();
+	~VulkanMaterial();
+
+	DECL_NO_COPY(VulkanMaterial);
+
+	virtual void setShader(const std::string& shaderFileName, ShaderType type);
+	virtual void removeShader(ShaderType type);
+	virtual void setDiffuse(Color c);
+	virtual int compileMaterial(std::string& errString);
+	virtual void addConstantBuffer(std::string name, unsigned int location);
+	virtual void updateConstantBuffer(const void* data, size_t size, unsigned int location);
+	virtual int enable();
+	virtual void disable();
+private:
+
+};
+
