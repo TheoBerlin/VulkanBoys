84c19733ec86fb5d2a1a64db3b050814d9f86006
TheoBerlin <theo.holmqvist@gmail.com>
2020-02-04 16:59:54 +0100

    Per-frame cbuffers

--- testbench-master/gl_testbench/Vulkan/VulkanConstantBuffer.h
+++ testbench-master/gl_testbench/Vulkan/VulkanConstantBuffer.h
@@ -1,38 +1,44 @@
 #include "../ConstantBuffer.h"
+#include "Common.h"
 
 #include "Vulkan/vulkan.h"
 
 class VulkanRenderer;
 class VulkanDevice;
 
+struct PerFrameResources {
+    VkBuffer m_BufferHandle;
+    VkDeviceMemory m_BufferMemory;
+};
+
 class VulkanConstantBuffer : public ConstantBuffer
 {
 public:
-    VulkanConstantBuffer(std::string NAME, unsigned int location);
+    VulkanConstantBuffer(std::string NAME, unsigned int location, unsigned int currentFrame);
     ~VulkanConstantBuffer();
 
     // It is unknown when the buffer is initialized, therefore some resources need to be stored before that point
     void provideResources(VulkanRenderer* pRenderer, VulkanDevice* pVulkanDevice);
 
-    // Initialize the constant buffer instance with necessary vulkan resources
+    // Initialize the current frame's constant buffer instance with necessary vulkan resources
     void initialize(VkDeviceSize size);
 
     void setData(const void* data, size_t size, Material* m, unsigned int location);
     void bind(Material* material);
 
-	VkBuffer getBuffer() { return m_BufferHandle; }
+	VkBuffer getBuffer() { return m_PerFrameResources[m_CurrentFrame].m_BufferHandle; }
     uint32_t getLocation() { return m_Location; }
+
+    void setCurrentFrame(unsigned int frame) { m_CurrentFrame = frame; }
+
 private:
     std::string m_Name;
+    uint32_t m_Location;
 
-    // Used for an ugly hack. OpenGL doesn't need to know the size of constant buffers at creation, but Vulkan does.
-    // The size of the buffer is first found when setData is called, and if this handle is VK_NULL_HANDLE,
-    // the buffer is initialized.
-    VkBuffer m_BufferHandle;
+    PerFrameResources m_PerFrameResources[MAX_FRAMES_IN_FLIGHT];
 
     VulkanRenderer* m_pRenderer;
 	VulkanDevice* m_pDevice;
-	
-    VkDeviceMemory m_BufferMemory;
-    uint32_t m_Location;
+
+    unsigned int m_CurrentFrame;
 };
