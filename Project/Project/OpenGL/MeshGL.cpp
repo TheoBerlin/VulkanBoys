9816df1be49b181f9e247c8a9bf70eae1644b1a1
TheoBerlin <theo.holmqvist@gmail.com>
2020-02-05 14:20:44 +0100

    Remove openGL files

--- Project/Project/OpenGL/MeshGL.cpp
+++
@@ -1,6 +0,0 @@
-#include "MeshGL.h"
-
-// Mesh::DATA_USAGE is an enum { STATIC, DYNAMIC, DONTCARE };
-
-MeshGL::MeshGL() {}
-MeshGL::~MeshGL() {}
--- Project/Project/Renderer.cpp
+++ Project/Project/Renderer.cpp
@@ -1,13 +1,10 @@
-#include "OpenGL/OpenGLRenderer.h"
 #include "Vulkan/VulkanRenderer.h"
 #include "Renderer.h"
 
 
 Renderer* Renderer::makeRenderer(BACKEND option)
 {
-	if (option == BACKEND::GL45) {
-		return new OpenGLRenderer();
-	} else if (option == BACKEND::VULKAN) {
+	if (option == BACKEND::VULKAN) {
 		return new VulkanRenderer();
 	}
 }
